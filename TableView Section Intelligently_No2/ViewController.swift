9f49458a2e3afc85158bfe592e1407a806999f97
masato <masato@m-mbp.local>
2018-10-11 20:57:54 +0800

    Initial Commit

---
+++ TableView Section Intelligently_No2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TableView Section Intelligently_No2
+//
+//  Created by masato on 11/10/2018.
+//  Copyright © 2018 masato. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
